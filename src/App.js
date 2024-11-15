2209d86361037d65bafd8525d20773ba642c99af
Jiyoun Choi <jiyunjiyun0723@naver.com>
2024-11-15 19:46:14 +0900

    api setting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/HomePage/Homepage';
 
 function App() {
   return (
     <Routes>
-      <Route>
-        
-      </Route>
+      <Route path="/" element={<Homepage/>}></Route>
     </Routes>
   );
 }
